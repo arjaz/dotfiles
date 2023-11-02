b6ab428fa2c21c4cbdb1b86a64cab1a8f406017a
Eugene Rossokha <hsugeneyos@gmail.com>
2023-11-02 09:41:19 +0200

    Change the wallpaper

--- scripts/to-light-theme.sh
+++ scripts/to-light-theme.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-light' &&
-    feh --bg-fill --no-fehbg ~/pictures/wallpapers/wallhaven-ox33vm_2560x1440.png &&
+    feh --bg-fill --no-fehbg ~/pictures/wallpapers/wallhaven-96jdd8_2560x1440.png &&
     sh ~/.config/kitty/light.sh &&
     sh ~/.config/polybar/light.sh &&
     sh ~/dotfiles/rcs/eww/scripts/light.sh
