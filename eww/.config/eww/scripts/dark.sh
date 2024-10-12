f792b530b09240670436c098c8aca07de8da9a6c
Eugene Rossokha <hsugeneyos@gmail.com>
2024-10-12 11:30:53 +0300

    Remove eww

--- eww/.config/eww/scripts/dark.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -f ~/.config/eww/eww-dark.scss ]
-then
-    mv ~/.config/eww/eww.scss ~/.config/eww/eww-light.scss && mv ~/.config/eww/eww-dark.scss ~/.config/eww/eww.scss
-else
-    echo "nok"
-fi
--- eww/.config/eww/scripts/light.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -f ~/.config/eww/eww-light.scss ]
-then
-    mv ~/.config/eww/eww.scss ~/.config/eww/eww-dark.scss && mv ~/.config/eww/eww-light.scss ~/.config/eww/eww.scss
-else
-    echo "nok"
-fi
