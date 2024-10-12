7af5e70e09fb8c5bf255bdbeb21f73255d8f9a3e
Eugene Rossokha <hsugeneyos@gmail.com>
2024-10-12 11:33:01 +0300

    Rework theme monitoring

--- scripts/to-dark-theme.sh
+++ scripts/to-dark-theme.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env sh
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark' &&
-    feh --bg-fill --no-fehbg ~/pictures/wallpapers/frieren-anime-girl-3840x2160-15156.jpg &&
-    sh ~/dotfiles/polybar/.config/polybar/dark.sh &&
-    sh ~/dotfiles/rofi/.config/rofi/dark.sh
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
--- scripts/to-light-theme.sh
+++ scripts/to-light-theme.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env sh
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-light' &&
-    feh --bg-fill --no-fehbg ~/pictures/wallpapers/cropped-2560-1440-1342229.png &&
-    sh ~/dotfiles/polybar/.config/polybar/light.sh &&
-    sh ~/dotfiles/rofi/.config/rofi/light.sh
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
