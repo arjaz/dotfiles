2ac471ea54360d7961e5ea3e9c89d300d3f00245
Eugene Rossokha <hsugeneyos@gmail.com>
2024-11-30 10:23:32 +0200

    Remove kitty from dbus-monitor

--- scripts/dbus-monitor.sh
+++ scripts/dbus-monitor.sh
@@ -5,15 +5,13 @@ while read -r line; do
     if [[ "$line" =~ "color-scheme" ]]; then
         read -r next_line
         if [[ "$next_line" =~ uint32\ 1 ]]; then
-            feh --bg-fill --no-fehbg ~/pictures/wallpapers/frieren-anime-girl-3840x2160-15156.jpg
+            feh --bg-fill --no-fehbg ~/pictures/wallpapers/frieren-anime-girl-2560x1440-15156.jpg
             sh ~/dotfiles/polybar/.config/polybar/dark.sh
             sh ~/dotfiles/rofi/.config/rofi/dark.sh
-            kitty +kitten themes --reload-in=all --cache-age=-1 Modus Vivendi
         elif [[ "$next_line" =~ uint32\ 2 ]]; then
             feh --bg-fill --no-fehbg ~/pictures/wallpapers/cropped-2560-1440-1342229.png
             sh ~/dotfiles/polybar/.config/polybar/light.sh
             sh ~/dotfiles/rofi/.config/rofi/light.sh
-            kitty +kitten themes --reload-in=all --cache-age=-1 Modus Operandi Tinted
         fi
     fi
 done
