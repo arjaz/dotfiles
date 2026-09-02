62dac5c21cda33c1e7dd208ab7a89e163f12389c
Eugene Rossokha <hsugeneyos@gmail.com>
2026-09-02 23:26:27 +0300

    Change wallpaper

--- scripts/to-light-theme.sh
+++ scripts/to-light-theme.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-light' && \
+    feh --bg-fill --no-fehbg ~/pictures/nazuna.png && \
+    # feh --bg-fill --no-fehbg ~/pictures/nanakusa.png && \
     # feh --bg-fill --no-fehbg ~/pictures/halftone-wallhaven-qzv6rd_2560x1440.png && \
-    feh --bg-fill --no-fehbg ~/pictures/wallhaven-qzv6rd_2560x1440.png && \
+    # feh --bg-fill --no-fehbg ~/pictures/wallhaven-qzv6rd_2560x1440.png && \
+    # feh --bg-fill --no-fehbg ~/pictures/wallhaven-3l2zey_3840x2160.png && \
+    # feh --bg-fill --no-fehbg ~/pictures/wallhaven-yxqjj7.png && \
     # feh --bg-fill --no-fehbg ~/pictures/halftone-wallhaven-gpl8d3_2560x1440.png && \
     # feh --bg-fill --no-fehbg ~/pictures/wallhaven-gpl8d3_2560x1440.png && \
     # feh --bg-fill --no-fehbg ~/pictures/7ndi66_dithered.png && \
